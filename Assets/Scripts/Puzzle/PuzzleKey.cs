f2713ad0b196db2911eb0e9d29117d0164553687
Evrim <149576486+MrEvrim@users.noreply.github.com>
2024-08-03 21:14:19 +0300

    CutScene, Door Puzzle, Colliders.

---
+++ Assets/Scripts/Puzzle/PuzzleKey.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class PuzzleKey : MonoBehaviour
+{
+    public Animator animator; // Animatör bileşenine referans
+    public string triggerName = "KeyTriggered"; // Animasyonu tetiklemek için kullanılan trigger adı
+
+    private void OnTriggerEnter(Collider other)
+    {
+        // Eğer oyun objesi "Key" tag'ine sahipse
+        if (other.CompareTag("BUBİRKEYDİR"))
+        {
+            // Animasyonu tetikle
+            animator.SetTrigger("GateOpen");
+            Debug.Log("Anahtar bulundu, animasyon tetiklendi.");
+        }
+    }
+}
