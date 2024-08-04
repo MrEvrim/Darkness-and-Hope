1958a9b8732a7e30352107aa1b1afdc3454c7158
Evrim <149576486+MrEvrim@users.noreply.github.com>
2024-08-04 15:15:24 +0300

    CaveCutScene, CaveCutTrigger

---
+++ Assets/Scripts/CaveCut.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class CaveCut : MonoBehaviour
+{
+    public GameObject player; // Player objesi referansı
+    public GameObject objectToActivate; // Aktive edilecek obje
+
+    private void OnTriggerEnter(Collider other)
+    {
+        // Player objesi tespit edildiğinde
+        if (other.CompareTag("Player"))
+        {
+            // Player objesini deaktif et
+            player.SetActive(false);
+
+            // Başka bir objeyi aktive et
+            objectToActivate.SetActive(true);
+        }
+    }
+}
